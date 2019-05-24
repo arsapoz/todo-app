efa5a9f1ba9a372dd094117b55223cb92a8c3683
arsaaa93 <arsaaa93@gmail.com>
2019-05-24 17:25:42 +0200

    add todo component created

---
+++ src/components/AddTodo.vue
@@ -0,0 +1,40 @@
+<template>
+   <div id="app">
+      <v-container>
+    <v-form
+      ref="form"
+      v-model="valid"
+      lazy-validation
+    >
+    <v-layout align-center>
+        <v-flex md9>
+      <v-text-field
+        v-model="name"
+        :rules="nameRules"
+        label="Add todo"
+        required
+      ></v-text-field>
+      </v-flex>
+      <v-flex md3>
+      <v-btn
+        color=""
+        @click="reset"
+      >
+        Add
+      </v-btn>
+      </v-flex>
+      </v-layout>
+    </v-form>
+  </v-container>
+</div>
+</template>
+
+<script>
+export default {
+    name: "AddTodo"
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,22 @@
 <template>
-  <Todos></Todos>
+  <div>
+<!-- 
+    <h1 class="pa-5">Add todo</h1> -->
+    <AddTodo></AddTodo>
+
+    <h1 class="pa-5">Todos</h1>
+    <Todos></Todos>
+  </div>
 </template>
 
 <script>
   import Todos from '../components/Todos'
+  import AddTodo from '../components/AddTodo'
 
   export default {
     components: {
-      Todos
+      Todos,
+      AddTodo
     }
   }
 </script>
